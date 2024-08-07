e59577ee81616771f31b330ecdff38902e9d7bf9
Masie Junior Seremu <seremu3@gmail.com>
2024-08-07 18:43:27 +0200

    PokemonProviveder

--- pokemon/lib/widgets/pokemon_list_tile.dart
+++ pokemon/lib/widgets/pokemon_list_tile.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokemon/models/pokemon.dart';
+import 'package:pokemon/providers/pokemon_data_providers.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class PokemonListTile extends StatelessWidget {
+class PokemonListTile extends ConsumerWidget {
   final String pokemanURL;
   const PokemonListTile({super.key, required this.pokemanURL});
 
   @override
-  Widget build(BuildContext context) {
-    return _tile(
-      context,
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    final pokemon = ref.watch(
+      pokemonDataProvider(
+        pokemanURL,
+      ),
     );
+    return pokemon.when(data: (data) {
+      return _tile(
+        context,
+        false,
+        data,
+      );
+    }, error: (error, stackTrace) {
+      return Text(
+        "error: $error",
+      );
+    }, loading: () {
+      return _tile(
+        context,
+        true,
+        null,
+      );
+    });
   }
 
   Widget _tile(
     BuildContext context,
+    bool isLoading,
+    Pokemon? pokemon,
   ) {
-    return ListTile(
-      title: Text(
-        pokemanURL,
+    return Skeletonizer(
+      enabled: true,
+      child: ListTile(
+        title: Text(
+          pokemanURL,
+        ),
       ),
     );
   }
